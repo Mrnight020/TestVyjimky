3cd56295d74bb3052617384d146e3fdf811ade93
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-06-07 08:57:07 +0200

    Add files via upload

---
+++ Test/Test/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Test
+{
+    partial class AritmetickyPrumer
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 110);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 273);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SkyBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(403, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Výpis";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(3, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(673, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Aritmetický průměr se provede jen na záporných celých číslech menší jak maximální" +
+    " hodnota int\r\n jakékoliv jiné hodnoty se do toho nepočítají";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(43, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(680, 64);
+            this.panel1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(558, 226);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(144, 22);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Vypisovat Vyjímky";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // AritmetickyPrumer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(769, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AritmetickyPrumer";
+            this.ShowIcon = false;
+            this.Text = "AritmetickyPrumer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Test/Test/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class AritmetickyPrumer : Form
+    {
+        public AritmetickyPrumer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocet = 0;
+            double soucet = 0;
+
+            for(int i = 0; i< textBox1.Lines.Length;i++)
+            {
+                try
+                {
+                    if (int.Parse(textBox1.Lines[i]) < 0)
+                    {
+                        int cislo = int.Parse(textBox1.Lines[i]);
+                        checked { soucet += cislo; }
+                        pocet++;
+                    }
+                }
+                catch(FormatException a)
+                {
+                    if(checkBox1.Checked)MessageBox.Show("Na Řádku " + (i + 1) + " se vyskytla tady ta chyba : " + a.Message);
+                }
+                catch (OverflowException a)
+                {
+                    if (checkBox1.Checked) MessageBox.Show("Na Řádku " + (i + 1) + " se vyskytla tady ta chyba : " + a.Message);
+                }
+                catch (ArithmeticException a) // double je moc velky takze se tato vyjimka nejspis nikdy nespusti :)
+                {
+                    if (checkBox1.Checked) MessageBox.Show(a.Message); 
+                    pocet--;
+                }
+            }
+
+            try
+            {
+                if(pocet == 0)
+                {
+                    MessageBox.Show("Aritmeticky prumer : 0");
+                }
+                else
+                {
+                    double aritmeticky = checked(soucet / pocet);
+                    MessageBox.Show("Aritmeticky prumer :" + aritmeticky);
+                }
+            }
+            catch(DivideByZeroException a) // u double se deleni nulou vypise jako NaN takze se tato vyjimka nikdy nespusti :)
+            {
+                if (checkBox1.Checked) MessageBox.Show(a.Message); 
+            }
+        }
+    }
+}
